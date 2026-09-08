793ebcd3bcb397afc1bb6b5a9038f05d798442c8
Abhinav <abhinavsunil@hotmail.com>
2026-09-09 01:14:01 +0530

    Feat : added the multer middleware config for file sharing

--- src/controllers/video.controller.ts
+++ src/controllers/video.controller.ts
@@ -2,9 +2,32 @@ import { type Request, type Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const uploadVideo = async (req: Request, res: Response) => {
-  console.log('Hit the upload service');
-  console.log('data of req', req);
-  res.status(StatusCodes.OK).json({ message: 'Hit the upload service' });
+  if (!req.file) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      success: false,
+      error: {
+        code: 'VIDEO_FILE_MISSING',
+        type: 'ValidationError',
+        field: 'video',
+      },
+      message: 'No video file was uploaded',
+      data: {},
+    });
+  } else {
+    const path = req.file.path;
+    return res.status(StatusCodes.ACCEPTED).json({
+      success: true,
+      error: {},
+      message: 'Succesfully file was uploaded',
+      data: {
+        filename: req.file.filename,
+        originalName: req.file.originalname,
+        size: req.file.size,
+        mimetype: req.file.mimetype,
+        path: path,
+      },
+    });
+  }
 };
 
 export { uploadVideo };
