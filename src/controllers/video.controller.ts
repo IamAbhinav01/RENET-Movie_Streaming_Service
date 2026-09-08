a4cb1186964a7a3f77016bc6a9fd48db4008e722
Abhinav <abhinavsunil@hotmail.com>
2026-09-09 00:18:14 +0530

    Feat : going to add multer middleware

--- src/controllers/video.controller.ts
+++ src/controllers/video.controller.ts
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 
 const uploadVideo = async (req: Request, res: Response) => {
   console.log('Hit the upload service');
+  console.log('data of req', req);
   res.status(StatusCodes.OK).json({ message: 'Hit the upload service' });
 };
 
